71c3758a3dde78d5a0978eb4674fbc48c4738c93
RadStr <40957172+RadStr@users.noreply.github.com>
2020-10-11 21:49:57 +0200

    Add comments to the SubbandSplitterIFace

--- src/Rocnikovy_Projekt/SubbandSplitterIFace.java
+++ src/Rocnikovy_Projekt/SubbandSplitterIFace.java
@@ -15,14 +15,39 @@ public interface SubbandSplitterIFace {
 //	 a[2*k+1] = Im[k], 0<k<(n-1)/2
 //	 a[1] = Im[(n-1)/2]
     // TODO: tehle text dat do popisu funkce:  Result array should be set to 0s, the method sets only the indexes in result array which correspond to the subband, others aren't touched
+
+    /**
+     * Copies the fftMeasures array part corresponding to the specified subband to the result array (to the same positions).
+     * Method doesn't change other indices of the result array.
+     * @param fftMeasures
+     * @param subbandCount
+     * @param subband
+     * @param result
+     */
     public void getSubband(double[] fftMeasures, int subbandCount, int subband, double[] result);
 
+    /**
+     * Copies the fftMeasures array part corresponding to the specified subband to newly created array (to the same positions).
+     * It is equivalent to setting all other indices except the subbands one to 0.
+     * @param fftMeasures
+     * @param subbandCount
+     * @param subband
+     * @return
+     */
     default public double[] getSubband(double[] fftMeasures, int subbandCount, int subband) {          // TODO: Subband indexed from 0 ... so the last subband has number subbandCount - 1
         double[] result = new double[fftMeasures.length];
         getSubband(fftMeasures, subbandCount, subband, result);
         return result;
     }
 
+    /**
+     * Takes sum of the specified subband in the given fftMeasures array. The resulting sum is averaged (divided by the array length)
+     * and then multiplied by the length of the subband.
+     * @param fftMeasures
+     * @param subbandCount
+     * @param subband
+     * @return
+     */
     default public double getSubbandEnergy(double[] fftMeasures, int subbandCount, int subband) {
         Pair<Integer, Integer> indexes = getSubbandIndices(fftMeasures.length, subbandCount, subband);       // Code duplication (Take a look at bottom of interface)
         double energy = 0;
@@ -45,6 +70,13 @@ public interface SubbandSplitterIFace {
         return energy;
     }
 
+    /**
+     * Help method, which shouldn't be called from outside. Gets the start index and length for the given subband.
+     * @param arrayLen
+     * @param subbandCount
+     * @param subband
+     * @return
+     */
     public Pair<Integer, Integer> getSubbandIndices(int arrayLen, int subbandCount, int subband);
 
 

7758147f696c5f94c9e04766ba99ba3d39264f22
RadStr <40957172+RadStr@users.noreply.github.com>
2020-12-14 20:32:20 +0100

    Add private constructor to logger
    So we can't instantiate the class anymore

--- src/util/logging/MyLogger.java
+++ src/util/logging/MyLogger.java
@@ -5,6 +5,8 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class MyLogger {
+    private MyLogger() {}   // To make only static access possible
+
     private static File logFile = new File("LOG.log");
     private static PrintWriter logStream;
     private static int indentation = 0;

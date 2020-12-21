9a95cd4c0fbfda2bfae316dd4c9da4228cecc0d2
RadStr <40957172+RadStr@users.noreply.github.com>
2020-12-22 00:10:45 +0100

    Rename: Program -> ProgramExperimental

--- src/player/experimental/ProgramExperimental.java
+++ src/player/experimental/ProgramExperimental.java
@@ -8,7 +8,7 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.SwingUtilities;
 
-public class Program {
+public class ProgramExperimental {
     public static void main(String[] args) {
         SwingUtilities.invokeLater(new Runnable() {
             @Override

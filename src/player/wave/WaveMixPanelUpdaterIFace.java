c16f000cb9cb2fcc46d82cce0336d6c829e36861
RadStr <40957172+RadStr@users.noreply.github.com>
2021-01-04 17:07:51 +0100

    Split line

--- src/player/wave/WaveMixPanelUpdaterIFace.java
+++ src/player/wave/WaveMixPanelUpdaterIFace.java
@@ -1,7 +1,8 @@
 package player.wave;
 
 /**
- * This interface is used to propagate change in mixWithChannelsInSecondDim slider to the class in which performs the mixing and plays the result.
+ * This interface is used to propagate change in mixWithChannelsInSecondDim slider to the class in which
+ * performs the mixing and plays the result.
  */
 public interface WaveMixPanelUpdaterIFace {
     void update(int index, double newValue);
